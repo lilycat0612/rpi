47a7a2614a26acb65ab3f3d958c1304c106ef3a2
lilycat0612 <27803532@qq.com>
2017-07-25 10:48:47 +0000

    backup system

--- backup.sh
+++ backup.sh
@@ -4,9 +4,9 @@ sudo tar --exclude /home/pi/mnt --exclude /home/pi/Downloads -zcvf /backup/home.
 sudo tar -zcvf /backup/boot.tar.gz /boot
 sudo tar -zcvf /backup/etc.tar.gz /etc
 echo "Finish Backup"
-sudo git add *
-echo "git add"
-sudo git commit -m 'backup system'
-echo 'git commit'
-sudo git push origin master
+sudo git add * >> /backup/backup.txt
+echo "Finish Add"
+sudo git commit -m 'backup system' >> /backup/backup.txt
+echo 'Finish Commit'
+sudo git push origin master >> /backup/backup.txt
 echo "Finish push to GitHub"
